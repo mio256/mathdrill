5144650b418657b9d1e0832f20cbf6386048abee
mio256 <reiya@woorth.io>
2023-05-29 22:29:37 +0900

    :fire: loop/consecutive answer/for True

--- main.go
+++ main.go
@@ -10,16 +10,17 @@ func main() {
 	var n int
 	seed := time.Now().UnixNano()
 	r := rand.New(rand.NewSource(seed))
-	a := r.Intn(1000)
-	b := r.Intn(1000)
-
-	fmt.Printf("%v + %v = ", a, b)
-	now := time.Now()
-	fmt.Scan(&n)
-	if n == a+b {
-		fmt.Println("Correct!")
-		fmt.Printf("Time: %.2fs\n", time.Since(now).Seconds())
-	} else {
-		fmt.Println(a + b)
+	for {
+		a := r.Intn(1000)
+		b := r.Intn(1000)
+		fmt.Printf("%v + %v = ", a, b)
+		now := time.Now()
+		fmt.Scan(&n)
+		if n == a+b {
+			fmt.Println("Correct!")
+			fmt.Printf("Time: %.2fs\n", time.Since(now).Seconds())
+		} else {
+			fmt.Println(a + b)
+		}
 	}
 }
