fdf27f3381bc90e2da6de783c1c313d0f1205543
mio256 <reiya@woorth.io>
2023-05-29 17:23:45 +0900

    :sparkles: add/simple method/rand.Intn(1000)

--- main.go
+++ main.go
@@ -2,8 +2,22 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	var n int
+	seed := time.Now().UnixNano()
+	r := rand.New(rand.NewSource(seed))
+	a := r.Intn(1000)
+	b := r.Intn(1000)
+
+	fmt.Printf("%v + %v = ", a, b)
+	fmt.Scan(&n)
+	if n == a+b {
+		fmt.Println("Correct!")
+	} else {
+		fmt.Println(n)
+	}
 }
