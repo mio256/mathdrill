1ebfef70ff4f0c5e688ddf1ccc62cab5c63f9ff3
mio256 <reiya@woorth.io>
2023-06-01 11:53:40 +0900

    :fire: change loop/consective answer/for 1,10

--- main.go
+++ main.go
@@ -10,17 +10,19 @@ func main() {
 	var n int
 	seed := time.Now().UnixNano()
 	r := rand.New(rand.NewSource(seed))
-	for {
+	score := 0.0
+	for i := 0; i < 10; i++ {
 		a := r.Intn(1000)
 		b := r.Intn(1000)
 		fmt.Printf("%v + %v = ", a, b)
 		now := time.Now()
 		fmt.Scan(&n)
+		score = score + time.Since(now).Seconds()
 		if n == a+b {
 			fmt.Println("Correct!")
-			fmt.Printf("Time: %.2fs\n", time.Since(now).Seconds())
 		} else {
-			fmt.Println(a + b)
+			fmt.Println("Incorrect: ", a+b)
 		}
 	}
+	fmt.Println("Score: ", score)
 }
