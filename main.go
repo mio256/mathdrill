6e90f2d5e2269a5355afd63593868d9e88cb1c67
mio256 <reiya@woorth.io>
2023-06-01 12:16:16 +0900

    :sparkles: add,sub,mul/more type of calc/generateRandNum,askQuestion

--- main.go
+++ main.go
@@ -2,27 +2,51 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"math/rand"
 	"time"
 )
 
-func main() {
-	var n int
+func generateRandNum(digit int) (int, int) {
 	seed := time.Now().UnixNano()
 	r := rand.New(rand.NewSource(seed))
-	score := 0.0
-	for i := 0; i < 10; i++ {
-		a := r.Intn(1000)
-		b := r.Intn(1000)
-		fmt.Printf("%v + %v = ", a, b)
-		now := time.Now()
-		fmt.Scan(&n)
-		score = score + time.Since(now).Seconds()
-		if n == a+b {
-			fmt.Println("Correct!")
-		} else {
-			fmt.Println("Incorrect: ", a+b)
-		}
+	max := int(math.Pow10(digit))
+	a := r.Intn(max)
+	b := r.Intn(max)
+	return a, b
+}
+
+func askQuestion(question string, answer int) float64 {
+	var userAnswer int
+	now := time.Now()
+
+	fmt.Printf("%v = ", question)
+	fmt.Scan(&userAnswer)
+	if userAnswer == answer {
+		fmt.Println("Correct!")
+	} else {
+		fmt.Println("Incorrect. The correct answer is", answer)
 	}
-	fmt.Println("Score: ", score)
+	return time.Since(now).Seconds()
+}
+
+func add() float64 {
+	a, b := generateRandNum(3)
+	return askQuestion(fmt.Sprintf("%d + %d", a, b), a+b)
+}
+
+func sub() float64 {
+	a, b := generateRandNum(3)
+	return askQuestion(fmt.Sprintf("%d - %d", a, b), a-b)
+}
+
+func mul() float64 {
+	a, b := generateRandNum(2)
+	return askQuestion(fmt.Sprintf("%d * %d", a, b), a*b)
+}
+
+func main() {
+	fmt.Println(add())
+	fmt.Println(sub())
+	fmt.Println(mul())
 }
